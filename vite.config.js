5c704bd26f69285825ea71c9285a24c41581ede7
Mansi Vaidhya <mansivaidhya@bitcot.com>
2026-03-26 12:50:03 +0530

    file changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Advance-Campaign/',  // ← tumhara repo name
+  base: '/',  // ← tumhara repo name
 })
